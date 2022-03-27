47bb8cc05748765b6ab2eeef1fc01013e83382c6
sithum-dev <89240669+sithum-dev@users.noreply.github.com>
2022-03-28 01:17:05 +0530

    Impliment address shorten function

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,6 +3,7 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
@@ -87,7 +88,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  0xadasdasddsa....gfhfghfg
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
